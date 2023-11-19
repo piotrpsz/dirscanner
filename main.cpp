6e6f1be8d9da440973273c29c242291a2371c845
piotrpsz <piotr@beesoft.pl>
2023-11-19 20:10:26 +0100

    using clap to configure

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@
 #include <atomic>
 #include <vector>
 #include <regex>
+#include <cstddef>
+#include <system_error>
+#include <optional>
+#include <variant>
+#include "clap/clap.h"
 
 namespace fs = std::filesystem;
 
@@ -19,6 +24,24 @@ std::atomic_uint64_t total_file_counter{};
 std::atomic_uint64_t matched_dir_counter{};
 std::atomic_uint64_t matched_file_counter{};
 
+auto clap = Clap("dirscanner v. 0.1",
+                 Arg()
+                    .marker("-i")
+                    .promarker("--icase")
+                    .ordef(false),
+                 Arg()
+                    .marker("-r")
+                    .promarker("--recursive"),
+                 Arg()
+                    .marker("-d")
+                    .promarker("--dir"),
+                 Arg()
+                    .marker("-t")
+                    .promarker("--text"),
+                 Arg()
+                    .marker("-n")
+                    .promarker("--name")
+);
 
 bool is_readable(fs::file_status status) noexcept {
     try {
@@ -49,40 +72,50 @@ bool ends_with(std::string const& text, char c) {
     return *std::prev(std::end(text)) == c;
 }
 
-void read_dir_content(std::string const& fpath, std::regex const& rgx) noexcept {
-    if (auto dirp = opendir(fpath.c_str()); dirp) {
-        for (;;) {
-            auto const dp = readdir(dirp);
-//            readdir_r()
-            if (!dp) break;
+void iterate_dir(std::string const& dir, std::regex const& rgx) noexcept {
+    total_dir_counter++;
+    if (auto dirp = opendir(dir.c_str()); dirp) {
+        auto* entry_prev = reinterpret_cast<struct dirent*>(malloc(offsetof(struct dirent, d_name) + NAME_MAX + 1));
+        struct dirent* entry;
 
-            std::string const name{dp->d_name};
+        for (;;) {
+            if (readdir_r(dirp, entry_prev, &entry) != 0) {
+                auto const err = std::make_error_code(std::errc{errno});
+                fmt::print(stderr, "({}) {}\n", err.value(), err.message());
+                break;
+            }
+            if (!entry) break;
+
+            std::string name{entry->d_name};
             // no hidden, no . (current) and no .. (parent)
             if (name[0] == '.') continue;
 
-            auto fp{fpath};
-            if (!ends_with(fp, '/'))
-                fp.append("/");
+            auto fp{dir};
+            if (!ends_with(fp, '/')) fp.append("/");
             fp = fp.append(name);
 
             struct stat fstat{};
             if (0 == lstat(fp.c_str(), &fstat)) {
-                total_file_counter++;
-//                std::cout << fmt::format("{}\n", fpath) << std::flush;
                 if ((fstat.st_mode & S_IFREG) == S_IFREG) {
-                    tg.run([] {
-
-                    });
-//                    std::cout << fmt::format("{}\n", fpath) << std::flush;
-                } else if ((fstat.st_mode & S_IFDIR) == S_IFDIR) {
-                    total_dir_counter++;
-                    std::smatch smatch;
-                    if (std::regex_search(fpath, smatch, rgx) && smatch[0].matched) {
-                        matched_dir_counter++;
-                        fmt::print("[{}] {}\n", matched_dir_counter.load(), fpath);
-                    }
+                    // Perform in a dedicated tbb-task
                     tg.run([fp, rgx] {
-                        read_dir_content(fp, rgx);
+                        total_file_counter++;
+                        std::smatch smatch;
+                        if (std::regex_search(fp, smatch, rgx) && smatch[0].matched) {
+                            matched_file_counter++;
+                            fmt::print("{}\n", fp);
+                        }
+                    });
+                }
+                else if ((fstat.st_mode & S_IFDIR) == S_IFDIR) {
+                    // Perform in a dedicated tbb-task
+                    tg.run([fp, rgx] {
+                        std::smatch smatch;
+                        if (std::regex_search(fp, smatch, rgx) && smatch[0].matched) {
+                            matched_dir_counter++;
+                            fmt::print("{}\n", fp);
+                        }
+                        iterate_dir(fp, rgx);
                     });
                 }
             }
@@ -91,8 +124,11 @@ void read_dir_content(std::string const& fpath, std::regex const& rgx) noexcept 
     }
 }
 
-
-int main() {
+int main(int argn, char* argv[]) {
+    clap.parse(argn, argv);
+//    std::cout << clap << '\n';
+//    return 0;
+/*
     std::string text = R"(
 #include <vector>
 namespace fs = std::filesystem;
@@ -108,36 +144,52 @@ int Main(char** argc, int argn) {}
 avbrewer
 
 )";
+*/
 
 //    auto expr = R"(\b(main)\S*(\(.*\))\S*)";
 //    auto expr = R"(\b(.*)\b(main)\b(\(.*\)))";
-/*
-    auto expr = R"((brew))";
-    std::regex rgx(expr, std::regex_constants::ECMAScript | std::regex_constants::icase);
-    std::smatch smatch;
-    if (std::regex_search(text, smatch, rgx)) {
-        std::cout << "OK\n";
-        auto s0 = smatch[0];
-        auto pos = smatch.position();
 
-        std::cout << smatch[0] << "  => (" << pos << ")" << '\n';
-        std::cout << smatch[1] << '\n';
-        std::cout << smatch[2] << "  => (" << (std::distance(smatch[1].first, smatch[2].first)) << ")" <<'\n';
-    }
-    else {
-        std::cout << "nie znaleziono\n";
-    }
+//    auto expr = R"((brew))";
+//    std::regex rgx(expr, std::regex_constants::ECMAScript | std::regex_constants::icase);
+//    std::smatch smatch;
+//    if (std::regex_search(text, smatch, rgx)) {
+//        std::cout << "OK\n";
+//        auto s0 = smatch[0];
+//        auto pos = smatch.position();
+//
+//        std::cout << smatch[0] << "  => (" << pos << ")" << '\n';
+//        std::cout << smatch[1] << '\n';
+//        std::cout << smatch[2] << "  => (" << (std::distance(smatch[1].first, smatch[2].first)) << ")" <<'\n';
+//    }
+//    else {
+//        std::cout << "nie znaleziono\n";
+//    }
+//
+//    return 0;
 
-    return 0;
-*/
 
-//    fs::path fpath{"/Users/piotr/Projects"};
+//    fs::path fpath{"/Users/piotr"};
     fs::path fpath{"/"};
-    auto expr = R"((brew))";
-    std::regex rgx(expr, std::regex_constants::ECMAScript); // | std::regex_constants::icase);
+    auto expr = R"(\b(Brew)\b)";
+    std::string name{};
+
+    auto rgx_flags = std::regex_constants::ECMAScript;
+
+    if (auto arg = clap["--icase"]; arg)
+        if (auto flag = std::get_if<bool>(&arg->value()); flag)
+            if (*flag)
+                rgx_flags |= std::regex_constants::icase;
+
+    if (auto arg = clap["--name"]; arg)
+        if (auto value = std::get_if<std::string>(&arg->value()); value)
+            name = *value;
+
+    fmt::print("name: {}\n", name);
+
+    std::regex rgx(expr, rgx_flags);
 
     tg.run([fpath, rgx] {
-        read_dir_content(fpath, rgx);
+        iterate_dir(fpath, rgx);
     });
 
     auto dt = share::execution_timer([&] {
